07d5a4b5598e53f8a76dac6a20fa87c8f4c39f94
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2022-02-02 19:58:09 +0000

    fixes for doc parsing

--- ir_datasets/datasets/wt10g.py
+++ ir_datasets/datasets/wt10g.py
@@ -27,10 +27,53 @@ QREL_DEFS = {
 }
 
 
-from .gov import GovDocs
+from .gov import GovDoc, GovDocs
 
 class WTDocs(GovDocs):
-    pass
+    
+    def docs_namespace(self):
+        return NAME
+
+    def _extract_next_block(self, inp, START, END):
+        # if START and END appear in inp, then return (everything after END in inp, the content between START and END),
+        # or if they don't appear, return (inp, None).
+        i_start = inp.find(START)
+        i_end = inp[i_start:].find(END)
+        #print(START, END, i_start, i_end)
+        if i_start == -1 or i_end == -1:
+            return inp.strip(), None
+        i_end += i_start
+        return inp[i_end+len(END): ], inp[i_start+len(START):i_end]
+
+    def _docs_iter(self):
+        dirs = sorted(Path(self.docs_dlc.path()).glob('WTX???'))
+        for source_dir in dirs:
+            for source_file in sorted(source_dir.glob('*.gz')):
+                yield from self._docs_ctxt_iter_gov(source_file)
+
+    def _process_gov_doc(self, raw_doc):
+        # read the file by exploiting the sequence of blocks in the document -- this ends
+        # up being several times faster than reading line-by-line
+        #print(raw_doc)
+        raw_doc, doc_id = self._extract_next_block(raw_doc, b'<DOCNO>', b'</DOCNO>')
+        assert doc_id is not None
+        doc_id = doc_id.strip().decode()
+        doc_body, doc_hdr = self._extract_next_block(raw_doc, b'<DOCHDR>', b'</DOCHDR>')
+        assert doc_hdr is not None
+        for encoding in ['utf8', 'ascii', 'latin1']:
+            try:
+                doc_url, doc_hdr = doc_hdr.decode(encoding).split('\n', 1)
+                break
+            except UnicodeDecodeError:
+                continue
+        content_type_match = re.search('^content-type:(.*)$', doc_hdr, re.I|re.M)
+        content_type = 'text/html' # default to text/html
+        if content_type_match:
+            content_type = content_type_match.group(1)
+            if ';' in content_type:
+                content_type, _ = content_type.split(';', 1)
+        content_type = content_type.strip()
+        return GovDoc(doc_id, doc_url, doc_hdr, bytes(doc_body), content_type)
 
 
 def _init():
@@ -39,12 +82,12 @@ def _init():
     dlc = DownloadConfig.context(NAME, base_path)
     subsets = {}
 
-    collection = GovDocs(dlc['docs'])
+    collection = WTDocs(dlc['docs'])
     base = Dataset(collection, documentation('_'))
 
     subsets['trec-web-2000'] = Dataset(
         collection,
-        TrecQueries(GzipExtract(dlc['trec-web-2000/queries']), namespace='gov/trec-web-2000', lang='en'),
+        TrecQueries(GzipExtract(dlc['trec-web-2000/queries']), namespace='wt10g/trec-web-2000', lang='en'),
         TrecQrels(GzipExtract(dlc['trec-web-2000/qrels']), QREL_DEFS),
         documentation('trec-web-2000')
     )
